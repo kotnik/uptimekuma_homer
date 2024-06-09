5d607a37e02b57e1415eb7ad6e6282bf8f08b3c8
Nikola Kotur <kotnick@gmail.com>
2024-06-09 18:11:02 +0200

    Reformat

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import os
 import sys
 
 import requests
-from flask import Flask
-from flask import jsonify
+from flask import Flask, jsonify
 from flask_cors import CORS, cross_origin
 from prometheus_client.parser import text_string_to_metric_families
 
@@ -16,18 +15,18 @@ app.config["CORS_HEADERS"] = "Content-Type"
 
 def query_uptimekuma():
     # Prepare
-    host = os.getenv('UKH_HOST', None)
-    api_key = os.getenv('UKH_API_KEY', None)
+    host = os.getenv("UKH_HOST", None)
+    api_key = os.getenv("UKH_API_KEY", None)
     if host is None or api_key is None:
-        raise Exception('Missing credentials')
-    if not host.endswith('/'):
+        raise Exception("Missing credentials")
+    if not host.endswith("/"):
         host = f"{host}/"
     host_metrics = f"{host}metrics"
 
     # Query
-    r = requests.get(host_metrics, auth=('', api_key))
+    r = requests.get(host_metrics, auth=("", api_key))
     if r.status_code != 200:
-        raise Exception('Failed to fetch')
+        raise Exception("Failed to fetch")
 
     # Parse
     services = {
@@ -38,31 +37,38 @@ def query_uptimekuma():
         "maintenance": 0,
     }
     for family in text_string_to_metric_families(r.text):
-        if family.name != 'monitor_status':
+        if family.name != "monitor_status":
             continue
         for sample in family.samples:
             value = int(sample.value)
-            services['total'] += 1
+            services["total"] += 1
             # (1 = UP, 0= DOWN, 2= PENDING, 3= MAINTENANCE)
             if value == 0:
-                services['down'] += 1
+                services["down"] += 1
             elif value == 1:
-                services['up'] += 1
+                services["up"] += 1
             elif value == 2:
-                services['pending'] += 1
+                services["pending"] += 1
             elif value == 3:
-                services['maintenance'] += 1
-    assert(services['total'] == (services['down'] + services['up'] + services['pending'] + services['maintenance']))
+                services["maintenance"] += 1
+    assert services["total"] == (
+        services["down"]
+        + services["up"]
+        + services["pending"]
+        + services["maintenance"]
+    )
 
     # Prepare output
     ret = {}
-    if services['down'] > 0:
-        ret['style'] = 'is-danger'
-        ret['title'] = 'Service disruption'
+    if services["down"] > 0:
+        ret["style"] = "is-danger"
+        ret["title"] = "Service disruption"
     else:
-        ret['style'] = 'is-success'
-        ret['title'] = 'Everything is up and running'
-    ret['content'] = f"<b>Online {services['up']} / {services['total']}. Check <a href={host} target=_blank>here</a> for more info. </br> <p>"
+        ret["style"] = "is-success"
+        ret["title"] = "Everything is up and running"
+    ret["content"] = (
+        f"<b>Online {services['up']} / {services['total']}. Check <a href={host} target=_blank>here</a> for more info. </br> <p>"
+    )
     return ret
 
 
@@ -73,6 +79,5 @@ def status():
 
 
 if __name__ == "__main__":
-    port = int(os.getenv('UKH_PORT', 8099))
+    port = int(os.getenv("UKH_PORT", 8099))
     app.run(host="0.0.0.0", port=port)
-
